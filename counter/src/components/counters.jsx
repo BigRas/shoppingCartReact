4258a0fdd2f3e14231a1fd4c6bfab525a472995b
Ras Amirzadeh <am.rasoul@gmail.com>
2019-12-12 21:42:16 +1100

    New commit from windows

---
+++ counter/src/components/counters.jsx
@@ -0,0 +1,31 @@
+import React, { Component } from "react";
+import Counter from "./counter";
+class Counters extends Component {
+  state = {
+    counters: [
+      { id: 1, value: 4 },
+      { id: 2, value: 0 },
+      { id: 3, value: 1 },
+      { id: 4, value: 2 }
+    ]
+  };
+  deleteHandler = counterId => {
+    const counters = this.state.counters.filter(c => c.id !== counterId);
+    this.setState({ counters: counters });
+  };
+  render() {
+    return (
+      <div>
+        {this.state.counters.map(counter => (
+          <Counter
+            key={counter.id}
+            onDelete={this.deleteHandler}
+            counter={counter}
+          />
+        ))}
+      </div>
+    );
+  }
+}
+
+export default Counters;
